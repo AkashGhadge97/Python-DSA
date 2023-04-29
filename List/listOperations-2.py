5f8b0e71294dc65b0426cc81bc24f5c2e730fce6
aaghadge97 <aaghadge97@gmail.com>
2023-04-29 14:51:38 +0530

    Added list operations

---
+++ List/listOperations-2.py
@@ -0,0 +1,39 @@
+# + Operator
+a = [1,2,3]
+b = [4,5,6]
+c = a+b
+print(c)
+
+# * Operator
+l = [1,2]
+l = l * 4
+print(l)
+
+#sum() fn
+lst = [3,4,5,6,7]
+print(sum(lst))
+
+#max() fn
+print(max(lst))
+
+#min() fn
+print(min(lst))
+
+#String and List
+#Convert string to a list
+st = 'game'
+lst = list(st)
+print(lst)
+
+#Split a string
+st = 'I love india'
+lst = st.split()
+print(lst)
+
+#Using a delimter to split a string
+st = 'You-are-nice'
+lst =  st.split('-')
+print(lst)
+
+#Using join() to join a list
+print(' '.join(lst))
