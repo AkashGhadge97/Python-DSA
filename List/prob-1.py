142ccbeb37380cd13891b51bfe1185ca6f0998f7
= <=>
2023-05-14 12:27:17 +0530

    Updated

---
+++ List/prob-1.py
@@ -0,0 +1,12 @@
+#Given 2D list calculate the sum of diagonal elements.
+
+def diagonal_sum(matrix):
+    sum=0
+    i=0
+    for row in matrix:
+        sum+=row[i]
+        i+=1
+    return sum
+
+myList2D= [[1,2,3],[4,5,6],[7,8,9]] 
+print("Diagonal Sum :", diagonal_sum(myList2D))
