2c4614cff70ecb4c5b4ec93da4d973916a600c43
= <=>
2023-05-14 12:46:16 +0530

    Updated

---
+++ List/prob-2.py
@@ -0,0 +1,9 @@
+#Given a list, write a function to get first, second best scores from the list.List may contain duplicates.
+
+def first_second(my_list):
+    my_list.sort(reverse=True)
+    return my_list[0],my_list[1]
+
+myList = [84,85,86,87,85,90,85,83,23,45,84,1,2,0]
+
+print(first_second(myList)) 
