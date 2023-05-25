91a7c7b947b80c2a26ec7d92aece7d7ddad6f842
Akash Ghadge <30373328+aaghadge97@users.noreply.github.com>
2023-05-26 00:01:40 +0530

    Update prob-5.py

--- List/prob-5.py
+++ List/prob-5.py
@@ -1,6 +1,6 @@
 #Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 #Input: nums = [1,2,3,1]
-#Output: true
+#Output: true 
 
 def contains_duplicate(nums):
     toSet = set(nums)
