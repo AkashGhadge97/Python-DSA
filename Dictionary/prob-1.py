b793cbff60e99f63fca193c5f7d4e8c89629c843
Akash Ghadge <30373328+aaghadge97@users.noreply.github.com>
2023-05-28 18:05:02 +0530

    Update prob-1.py

--- Dictionary/prob-1.py
+++ Dictionary/prob-1.py
@@ -15,3 +15,11 @@ def count_word_frequency(words):
 
 words = ['apple', 'orange', 'banana', 'apple', 'orange', 'apple']
 print(count_word_frequency(words))
+
+#Solution-2
+
+def count_word_frequency(words):
+    word_count = {}
+    for word in words:
+        word_count[word] = word_count.get(word, 0) + 1
+    return word_count
