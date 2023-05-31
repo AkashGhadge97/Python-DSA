267753aeb29b5f170d8ca05f1d357cda5abad663
Akash Ghadge <30373328+aaghadge97@users.noreply.github.com>
2023-05-31 23:57:20 +0530

    Update prob-4.py

--- Tuple/prob-4.py
+++ Tuple/prob-4.py
@@ -1,7 +1,7 @@
 # Write a function that takes a tuple of strings and concatenates them, separating each string with a space.
 
 def concatenate_strings(input_tuple):
-    return ' '.join(list(input_tuple))
+    return ' '.join(input_tuple)
 
 
 input_tuple = ('Hello', 'World', 'from', 'Python')
