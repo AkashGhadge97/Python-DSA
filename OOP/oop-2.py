c455e5bc1850ee24893043c857bd1a5269ded48c
Akash Ghadge <30373328+aaghadge97@users.noreply.github.com>
2023-06-25 00:10:40 +0530

    Create oop-2.py

---
+++ OOP/oop-2.py
@@ -0,0 +1,9 @@
+class StarCookie:
+    def __init__(self, color, weight):
+        self.color = color
+        self.weight = weight
+
+
+star_cookie1 = StarCookie("Red", 60)
+print(star_cookie1.color)
+print(star_cookie1.weight)
