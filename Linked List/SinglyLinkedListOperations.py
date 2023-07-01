2cc278f76f8bff7fe0c222c1be595600e7eab3d4
Akash Ghadge <30373328+AkashGhadge97@users.noreply.github.com>
2023-07-01 13:13:25 +0530

    Create SinglyLinkedListOperations.py

---
+++ Linked List/SinglyLinkedListOperations.py
@@ -0,0 +1,45 @@
+# Clas to create a new node
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+
+    # Create Empty linked list
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    # Print the linked list
+    def __str__(self):
+        temp_node = self.head
+        result = ''
+        while temp_node is not None:
+            result += str(temp_node.value)
+            if temp_node.next is not None:
+                result += ' -> '
+            temp_node = temp_node.next
+        return result
+
+    # Append a node at the end of the linked list
+    def append(self, value):
+        new_node = Node(value)
+
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+
+
+new_linked_list = LinkedList()
+new_linked_list.append(10)
+new_linked_list.append(20)
+new_linked_list.append(30)
+print(new_linked_list.__str__())
